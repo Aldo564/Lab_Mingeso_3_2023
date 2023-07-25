c51c3d2783a29320c6bc53101ebd37491f296697
Aldo <hola564@hotmail.es>
2023-07-24 21:22:06 -0400

    Arreglos menores de versiones

--- Backend/src/main/java/PythonTest/Backend/BackendApplication.java
+++ Backend/src/main/java/PythonTest/Backend/BackendApplication.java
@@ -3,9 +3,11 @@ package PythonTest.Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaRepositories
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- Backend/src/main/java/PythonTest/Backend/Entities/PyTestEntity.java
+++ Backend/src/main/java/PythonTest/Backend/Entities/PyTestEntity.java
@@ -1,6 +1,5 @@
 package PythonTest.Backend.Entities;
 
-
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
